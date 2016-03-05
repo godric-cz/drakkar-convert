5163efa370a75ff65e088122f67b7933749f668a
Godric <godric@korh.cz>
2016-03-05 15:03:46 +0100

    parametr pro určení výstupu

--- convert.php
+++ convert.php
@@ -4,15 +4,17 @@ namespace Drakkar;
 
 require_once 'vendor/autoload.php';
 
-$opt = getopt('v:dto'); // TODO zdokumentovat
+$opt = getopt('v:dto:b'); // TODO zdokumentovat
 
+$out = 'out';
 $k = new Konvertor;
 if(isset($opt['d'])) $k->debug(true);
 if(isset($opt['t'])) $k->zachovatTagy(true);
-if(isset($opt['o'])) $k->bezObrazku(true);
+if(isset($opt['b'])) $k->bezObrazku(true);
+if(isset($opt['o'])) $out = $opt['o'];
 foreach(glob('in/*.html') as $f) {
   preg_match('@_(\d\d)(_|\.)@', basename($f), $m);
   $vydani = $m[1];
   if(isset($opt['v']) && $opt['v'] != $vydani) continue;
-  $k->preved($f, 'out/' . $vydani);
+  $k->preved($f, $out . '/' . $vydani);
 }
