46166ba520860d37876ded0f88faae1a75170baa
Godric <godric@korh.cz>
2020-02-05 15:26:36 +0100

    odstranění &shy; při dělání url

--- src/_functions.php
+++ src/_functions.php
@@ -11,10 +11,14 @@ function str_contains($haystack, $needle) {
 function slugify($r) {
     $sDia = "ÁÄČÇĎÉĚËÍŇÓÖŘŠŤÚŮÜÝŽáäčçďéěëíňóöřšťúůüýž";
     $bezDia = "aaccdeeeinoorstuuuyzaaccdeeeinoorstuuuyz";
+    $ilegalni = [html_entity_decode('&shy;')];
+
+    $r = strtr($r, array_fill_keys($ilegalni, ''));
     $r = iconv('utf-8', 'Windows-1250//IGNORE', $r);
     $r = strtr($r, iconv('utf-8', 'Windows-1250', $sDia), $bezDia);
     $r = preg_replace('@[^a-zA-Z0-9\-]+@', '-', $r);
     $r = trim($r, '-');
     $r = strtolower($r);
+
     return $r;
 }
